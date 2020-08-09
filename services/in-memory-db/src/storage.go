1fe5ba26cdeeb3e1142214c3d0530f27eca1817d
Bruno Hautzenberger <bhautzenberger@gmail.com>
2020-08-09 18:31:08 +0000

    first implementation of Storage

--- services/in-memory-db/src/storage.go
+++ services/in-memory-db/src/storage.go
@@ -7,28 +7,112 @@ import (
 
 type StorageInterface interface {
 	Initialize()
-	Get() (bool, string, error)
-	Set(key string, value string)
-	Delete() error
-	Keys() []string
+	Get(realmName string, key string) (bool, string)
+	Set(realmName string, key string, value string, expiresIn int)
+	Delete(realmName string, key string) bool
+	Keys(realmName string) []string
+	Realms() []string
 }
 
 type Storage struct {
-	Values map[string]string
+	Data map[string]map[string]string
 }
 
 func (s *Storage) Initialize() {
-	s.Values = make(map[string]string)
+	s.Data = make(map[string]map[string]string)
 }
 
-func (s *Storage) Set(key string, value string, expiresIn int) {
-	s.Values[key] = value
+func (s *Storage) GetRealm(realm string) (bool, map[string]string) {
+	if _, ok := s.Data[realm]; ok {
+		return true, s.Data[realm]
+	}
+
+	return false, make(map[string]string)
+}
+
+func (s *Storage) CreateRealm(realm string) map[string]string {
+	if _, ok := s.Data[realm]; ok {
+		return s.Data[realm]
+	}
+
+	s.Data[realm] = make(map[string]string)
+
+	return s.Data[realm]
+}
+
+func (s *Storage) CleanEmptyRealm(realmName string) {
+	if realm, ok := s.Data[realmName]; ok {
+		if len(realm) == 0 {
+			delete(s.Data, realmName)
+		}
+	}
+}
+
+func (s *Storage) Get(realmName string, key string) (bool, string) {
+	ok, realm := s.GetRealm(realmName)
+	if !ok {
+		return false, ""
+	}
+
+	if val, ok := realm[key]; ok {
+		return true, val
+	}
+
+	return false, ""
+}
+
+func (s *Storage) Set(realmName string, key string, value string, expiresIn int) {
+	ok, realm := s.GetRealm(realmName)
+	if !ok {
+		realm = s.CreateRealm(realmName)
+	}
+
+	realm[key] = value
 
 	expireFunc := func() {
-		delete(s.Values, key)
+		delete(realm, key)
+		s.CleanEmptyRealm(realmName)
 		log.Printf("Deleted key %v after %v seconds\n", key, expiresIn)
 	}
 
 	time.AfterFunc(time.Duration(expiresIn)*time.Second, expireFunc)
 	log.Printf("Set key %v. It will Expire in %v seconds\n", key, expiresIn)
+}
+
+func (s *Storage) Delete(realmName string, key string) bool {
+	ok, realm := s.GetRealm(realmName)
+	if !ok {
+		return false
+	}
+
+	if _, ok := realm[key]; ok {
+		delete(realm, key)
+		s.CleanEmptyRealm(realmName)
+		return true
+	}
+
+	return false
+}
+
+func (s *Storage) Keys(realmName string) []string {
+	ok, realm := s.GetRealm(realmName)
+	if !ok {
+		return make([]string, 0)
+	}
+
+	keys := make([]string, 0, len(realm))
+	for k := range realm {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+func (s *Storage) Realms() []string {
+	keys := make([]string, 0, len(s.Data))
+	for k := range s.Data {
+		keys = append(keys, k)
+	}
+
+	return keys
 }
